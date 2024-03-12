d2639afe35ed945626fbb732c85de9bfe38c9c29
Ethan <ethan.scott2@icloud.com>
2024-03-12 09:32:35 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Ethan Scott on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
